c244a9b19497bc4919a8079b6a9f803f4ab87eef
Israel <israelcaldas10@gmail.com>
2026-03-12 06:17:03 -0300

    save

--- ft_array_to_list.c
+++ ft_array_to_list.c
@@ -6,11 +6,31 @@
 /*   By: icaldas- <icaldas-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/10 23:08:57 by icaldas-          #+#    #+#             */
-/*   Updated: 2026/03/12 04:10:52 by icaldas-         ###   ########.fr       */
+/*   Updated: 2026/03/12 05:48:42 by icaldas-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_push_swap.h"
+
+static void	ft_put_index(t_stack **head, int *ordered_array)
+{
+	int		i;
+	t_stack	**aux;
+
+	aux = head;
+	while ((*aux)->next)
+	{
+		i = 0;
+		while (ordered_array[i] != (*aux)->value)
+			i++;
+		(*aux)->index = i + 1;
+		aux = &(*aux)->next;
+	}
+	i = 0;
+	while (ordered_array[i] != (*aux)->value)
+		i++;
+	(*aux)->index = i + 1;
+}
 
 static void	ft_put_position(t_stack **head)
 {
@@ -28,7 +48,7 @@ static void	ft_put_position(t_stack **head)
 	(*aux)->pos = i;
 }
 
-t_stack *ft_array_to_list(int argc, int *unordered_array)
+t_stack *ft_array_to_list(int argc, int *unordered_array, int *ordered_array)
 {
 	int		i;
 	t_stack	*stack_a;
@@ -44,6 +64,7 @@ t_stack *ft_array_to_list(int argc, int *unordered_array)
 		argc--;
 	}
 	ft_put_position(&stack_a);
+	ft_put_index(&stack_a, ordered_array);
 	return (stack_a);
 }
 
@@ -51,18 +72,21 @@ int	main(void)
 {
 	t_stack	*stack_a;
 	t_stack	*aux;
-	int		a[] = {1, 3, 5, 2, 4, 6};
+	int		a[] = {2, 6, 10, 4, 8, 12};
+	int		a_ordered[] = {2, 4, 6, 8, 10, 12};
 	int		argc = 7;
 
-	stack_a = ft_array_to_list(argc, a);
+	stack_a = ft_array_to_list(argc, a, a_ordered);
 	aux = stack_a;
 	while (aux->next)
 	{
 		printf("Value: %d\n", aux->value);
 		printf("Pos: %d\n", aux->pos);
+		printf("Index: %d\n\n", aux->index);
 		aux = aux->next;
 	}
 	printf("Value: %d\n", aux->value);
 	printf("Pos: %d\n", aux->pos);
+	printf("Index: %d\n", aux->index);
 	return (0);
 }
